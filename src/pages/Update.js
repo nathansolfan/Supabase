702feea96efe928b84b6f5dbaf807b9583cc8b62
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2023-12-08 23:59:39 +0000

    Insert standard form

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -34,7 +34,36 @@ const Update = () => {
 
   return (
     <div className="page update">
-      <form action=""></form>
+      <form action="">
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Method:</label>
+        <input
+          type="text"
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Create test</button>
+
+        {/* if error, display */}
+        {formError && <p className="error">{formError}</p>}
+      </form>
     </div>
   );
 };
